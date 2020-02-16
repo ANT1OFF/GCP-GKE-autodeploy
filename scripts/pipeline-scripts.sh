c669f26d810d14df7abf0aa05883a761c9c3cd38
Johan <johanselnes2013@gmail.com>
2020-02-16 10:09:24 +0100

    moved more stuff from pipline to script

--- scripts/pipeline-scripts.sh
+++ scripts/pipeline-scripts.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-
 gcpauth() {
-   echo ${GCP_SECRET} > secret.json
-   gcloud auth activate-service-account --key-file secret.json
-   gcloud config set project ${PROJECT}
+    echo "${GCP_SECRET}" > secret.json
+    gcloud auth activate-service-account --key-file secret.json
+    gcloud config set project "${PROJECT}"
 }
 
 deployCluster() {
-  echo "TEST A $1";
-}
+    gcpauth
+
+    set +e  # turn off error-trapping
+    gcloud deployment-manager deployments describe "${CLUSTER_NAME}";
+    echo $? > cluster-present.txt
+    set -e  # turn on error-trapping
+
+    if [[ $(cat cluster-present.txt) -ne 0 ]]; then
+        gcloud deployment-manager deployments create "${CLUSTER_NAME}" --config ./deployment/cluster.yaml;
+        else
+        gcloud deployment-manager deployments update "${CLUSTER_NAME}" --config ./deployment/cluster.yaml;
+    fi
+} 
 
 deployService() {
   echo "TEST B $2";
 }
 
 
-# calls function passed as an argument. Case sensitive
+# calls function passed as an argument verbatim. Case sensitive
 "$@"
 
