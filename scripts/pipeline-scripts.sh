c295088f69b35d0abf568c12e71f1e86c9fa6adb
Johan <johanselnes2013@gmail.com>
2020-02-16 09:10:40 +0100

    testing change to pipeline

---
+++ scripts/pipeline-scripts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+gcpauth() {
+   echo ${GCP_SECRET} > secret.json
+   gcloud auth activate-service-account --key-file secret.json
+   gcloud config set project ${PROJECT}
+}
+
+deployCluster() {
+  echo "TEST A $1";
+}
+
+deployService() {
+  echo "TEST B $2";
+}
+
+
